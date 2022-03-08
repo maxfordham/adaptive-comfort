3f47aa299fa3bab5dd14adf122be16df19b5ea3a
olly <o.hensby@maxfordham.com>
2022-03-08 14:19:28 +0000

    Fixed pytests and ran successfully

--- tests/constants.py
+++ tests/constants.py
@@ -4,9 +4,9 @@ import numpy as np
 DIR_TESTS = pathlib.Path(__file__).parent
 DIR_TESTJOB1_TM52 = DIR_TESTS / "testmodels" / "TestJob1" / "mf_results" / "tm52" / "data"
 DIR_TESTDATA = DIR_TESTS / "testdata"
-FPTH_IES_TESTJOB1_V_0_1 = DIR_TESTDATA / "tm52_vista_pro" / "ies_TestJob1_TM52_0_1.csv" 
-FPTH_IES_TESTJOB1_V_0_5 = DIR_TESTDATA / "tm52_vista_pro" / "ies_TestJob1_TM52_0_5.csv"
+FPTH_IES_TESTJOB1_V_0_1 = DIR_TESTS / "testmodels" / "TestJob1" / "vista" / "TestJob1_TM52_0.1.csv" 
+# FPTH_IES_TESTJOB1_V_0_5 = DIR_TESTDATA / "tm52_vista_pro" / "ies_TestJob1_TM52_0_5.csv"
 
 arr_max_adaptive_temp = np.load(str(DIR_TESTJOB1_TM52 / "arr_max_adaptive_temp.npy"))
 arr_running_mean_temp = np.load(str(DIR_TESTJOB1_TM52 / "arr_running_mean_temp.npy"))
-# arr_operative_temp = np.load(str(DIR_TESTJOB1_TM52 / "arr_operative_temp.npy"))
+arr_operative_temp = np.load(str(DIR_TESTJOB1_TM52 / "arr_operative_temp.npy"))
