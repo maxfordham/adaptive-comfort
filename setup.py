ebd5e90693b843e85d70043d2487db61b50234e7
olly <o.hensby@maxfordham.com>
2022-06-16 13:14:57 +0100

    Updated setup and requirements.txt

--- setup.py
+++ setup.py
@@ -4,46 +4,45 @@
 
 from setuptools import setup, find_packages
 
-with open("README.md") as readme_file:
-    readme = readme_file.read()
+if __name__ == '__main__':
+    setup()
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
-
-requirements = [
-    "click>=7.0",
-    "python==3.4.5",
-    "numpy>=1.14.0",
-    "pandas>=0.20.3",
-    "xlsxwriter>=3.0.3",
-]
-
-test_requirements = [
-    "pytest>=3",
-]
-
-setup(
-    author="Oliver James Hensby",
-    author_email="o.hensby@maxfordham.com",
-    python_requires=">=3.4.5",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-    ],
-    description="This package assesses the thermal comfort of a space within a building against the Criterion defined in CIBSE TM52 and TM59.",
-    entry_points={"console_scripts": ["adaptive_comfort=adaptive_comfort.cli:main",],},
-    install_requires=requirements,
-    long_description=readme + "\n\n" + history,
-    include_package_data=True,
-    keywords="adaptive_comfort",
-    name="adaptive_comfort",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    test_suite="tests",
-    tests_require=test_requirements,
-    url="https://github.com/maxfordham/adaptive-comfort",
-    version="0.1.1",
-)
+# with open("README.md") as readme_file:
+#     readme = readme_file.read()
+# 
+# 
+# requirements = [
+#     "pip<19.2",
+#     "numpy<=1.14.0",
+#     "pandas<=0.20.3",
+#     "xlsxwriter<=3.0.3",
+# ]
+# 
+# test_requirements = [
+#     "pytest>=3",
+# ]
+# 
+# 
+# setup(
+#     name="adaptive-comfort",
+#     version="0.1.1",
+#     author="Oliver James Hensby",
+#     author_email="o.hensby@maxfordham.com",
+#     description="This package assesses the thermal comfort of a space within a building against the Criterion defined in CIBSE TM52 and TM59.",
+#     keywords="adaptive-comfort",
+#     url="https://github.com/maxfordham/adaptive-comfort",
+#     classifiers=[
+#         "Development Status :: 2 - Pre-Alpha",
+#         "Intended Audience :: Developers",
+#         "Natural Language :: English",
+#         "Programming Language :: Python :: 3.4",
+#         "Programming Language :: Python :: 3.5",
+#     ],
+#     install_requires=requirements,
+#     package_dir={"": "src"},
+#     packages=find_packages(where="src"),
+#     python_requires="==3.4.5",
+#     test_suite="tests",
+#     tests_require=test_requirements,
+#     
+# )
