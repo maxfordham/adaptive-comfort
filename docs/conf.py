50ab9f6a02fc2136f07a48384629e7961fe5b0ce
olly <o.hensby@maxfordham.com>
2022-06-16 16:35:26 +0100

    Updating config for docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,82 +5,35 @@
 ###############################################################################
 import os
 
-
-author = "mfcode"
+author = 'Max Fordham LLP'
 autoapi_add_toctree_entry = True
-autoapi_dirs = ["../src"]
+autoapi_dirs = ['../src']
 autoapi_generate_api_docs = True
-autoclass_content = "both"
-bibtex_bibfiles = ["references.bib"]
-comments_config = {"hypothesis": False, "utterances": False}
-copyright = "2021"
-exclude_patterns = ["**.ipynb_checkpoints", ".DS_Store", "Thumbs.db", "_build"]
+autoclass_content = 'both'
+bibtex_bibfiles = ['references.bib']
+comments_config = {'hypothesis': False, 'utterances': False}
+copyright = '2022'
+exclude_patterns = ['**.ipynb_checkpoints', '.DS_Store', 'Thumbs.db', '_build']
 execution_allow_errors = False
 execution_excludepatterns = []
 execution_in_temp = False
 execution_timeout = 30
-extensions = [
-    "sphinx_togglebutton",
-    "sphinx_copybutton",
-    "myst_nb",
-    "jupyter_book",
-    "sphinx_thebe",
-    "sphinx_comments",
-    "sphinx_external_toc",
-    "sphinx.ext.intersphinx",
-    "sphinx_panels",
-    "sphinx_book_theme",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.todo",
-    "sphinx.ext.autosummary",
-    "autoapi.extension",
-    "sphinxcontrib.mermaid",
-    "sphinx_github_changelog",
-    "sphinxcontrib.bibtex",
-    "sphinx_jupyterbook_latex",
-]
+extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_design', 'sphinx_book_theme', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinx.ext.todo', 'sphinx.ext.autosummary', 'autoapi.extension', 'sphinxcontrib.mermaid', 'sphinx_github_changelog', 'sphinxcontrib.bibtex', 'sphinx_jupyterbook_latex']
 external_toc_exclude_missing = False
-external_toc_path = "_toc.yml"
-html_baseurl = ""
-html_favicon = "images/logo.ico"
-html_logo = "images/logo.png"
-html_sourcelink_suffix = ""
-html_theme = "sphinx_book_theme"
-html_theme_options = {
-    "search_bar_text": "Search this book...",
-    "launch_buttons": {
-        "notebook_interface": "classic",
-        "binderhub_url": "https://mybinder.org",
-        "jupyterhub_url": "",
-        "thebe": False,
-        "colab_url": "",
-    },
-    "path_to_docs": "docs",
-    "repository_url": "https://github.com/maxfordham/adaptive-comfort",
-    "repository_branch": "master",
-    "google_analytics_id": "",
-    "extra_navbar": 'Powered by <a href="https://jupyterbook.org">Jupyter Book</a>',
-    "extra_footer": "",
-    "home_page_in_toc": True,
-    "use_repository_button": True,
-    "use_edit_page_button": False,
-    "use_issues_button": True,
-}
-html_title = "adaptive_comfort"
-jupyter_cache = ""
-jupyter_execute_notebooks = "auto"
+external_toc_path = '_toc.yml'
+html_baseurl = ''
+html_favicon = 'images/logo.ico'
+html_logo = 'images/logo.png'
+html_sourcelink_suffix = ''
+html_theme = 'sphinx_book_theme'
+html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': 'https://mybinder.org', 'jupyterhub_url': '', 'thebe': False, 'colab_url': ''}, 'path_to_docs': 'docs', 'repository_url': 'https://github.com/maxfordham/adaptive-comfort', 'repository_branch': 'master', 'google_analytics_id': '', 'extra_navbar': 'Powered by <a href="https://jupyterbook.org">Jupyter Book</a>', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '', 'use_repository_button': True, 'use_edit_page_button': False, 'use_issues_button': True}
+html_title = 'adaptive-comfort'
+jupyter_cache = ''
+jupyter_execute_notebooks = 'auto'
 language = None
-latex_engine = "pdflatex"
-myst_enable_extensions = [
-    "colon_fence",
-    "dollarmath",
-    "linkify",
-    "substitution",
-    "tasklist",
-]
-myst_url_schemes = ["mailto", "http", "https"]
+latex_engine = 'pdflatex'
+myst_enable_extensions = ['colon_fence', 'dollarmath', 'linkify', 'substitution', 'tasklist']
+myst_url_schemes = ['mailto', 'http', 'https']
 napoleon_google_docstring = True
 napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
@@ -92,11 +45,10 @@ napoleon_use_admonition_for_references = False
 napoleon_use_ivar = True
 napoleon_use_param = True
 napoleon_use_rtype = True
-nb_output_stderr = "show"
+nb_output_stderr = 'show'
 numfig = True
-panels_add_bootstrap_css = False
-pygments_style = "sphinx"
+pygments_style = 'sphinx'
 sphinx_github_changelog_token = os.environ.get("GITHUB_TOKEN_CHANGELOG")
-suppress_warnings = ["myst.domains"]
+suppress_warnings = ['myst.domains']
 use_jupyterbook_latex = True
 use_multitoc_numbering = True
